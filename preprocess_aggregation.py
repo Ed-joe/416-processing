5745067daa6be5baf879fef5bcec3c0e29f681fc
rwillett9 <rjw4321@gmail.com>
2022-05-14 19:33:40 -0400

    louisiana data

--- preprocess_aggregation.py
+++ preprocess_aggregation.py
@@ -18,9 +18,9 @@ if not os.path.exists(args.d):
 
 # Import block data, rename columns to more readable form, and remove non-relevant data
 block_demographic_data = geopandas.read_file(args.block_demographics)
-census_to_demographic_name_map = {"P0020001": "TOTAL_POPULATION", "P0020002": "HISPANIC_LATINO", "P0020005": "WHITE",
-                                  "P0020006": "AFRICAN_AMERICAN", "P0020007": "AMERICAN_INDIAN", "P0020008": "ASIAN",
-                                  "P0020009": "PACIFIC_ISLANDER", "P0020010": "OTHER"}
+census_to_demographic_name_map = {"C_TOT20": "TOTAL_POPULATION", "C_HSP20": "HISPANIC_LATINO", "C_WHT20": "WHITE",
+                                  "CVAP_BLK20": "AFRICAN_AMERICAN", "C_AIA20": "AMERICAN_INDIAN", "C_ASN20": "ASIAN",
+                                  "C_NHP20": "PACIFIC_ISLANDER"}
 block_demographic_data = block_demographic_data.rename(columns=census_to_demographic_name_map)
 block_demographic_data = block_demographic_data.filter(["geometry"] + list(census_to_demographic_name_map.values()))
 
