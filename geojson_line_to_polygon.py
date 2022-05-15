6e637f7a0e7a0b6e4c3464aab415acbfa9160ef8
rwillett9 <rjw4321@gmail.com>
2022-05-15 15:59:37 -0400

    conver to polygon features

---
+++ geojson_line_to_polygon.py
@@ -0,0 +1,25 @@
+import argparse
+import geopandas
+from shapely.geometry import Polygon
+
+# Arguments Parsing
+parser = argparse.ArgumentParser(
+    description='Take in district shape data as geojson, and map precinct data to districts with an ID')
+parser.add_argument("-i", help="input file", required=True)
+parser.add_argument("-d", help="output file", required=True)
+args = parser.parse_args()
+
+geojson = geopandas.read_file(args.i)
+
+d = {'name': [], 'geometry': []}
+
+for i in range(len(geojson.geometry)):
+    # print(geojson)
+    d['name'].append(geojson.name[i])
+    d['geometry'].append(Polygon(geojson.geometry[i].coords))
+
+# print(d)
+
+new_geojson = geopandas.GeoDataFrame(d, crs="EPSG:4326")
+new_geojson.to_file(args.d, driver='GeoJSON')
+print('wrote to', args.d)
